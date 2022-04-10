0a7cb07ab13fc35b0c3a96f1b3c8c35202a2910c
yuchengfan <yuchengfan321@163.com>
2022-04-10 17:02:08 +0800

    eureka-ribbon-Hystrix实现熔断限流

--- parent-cloud/Riboon-eureka-consumer/src/main/java/com/unv/service/RibbonService.java
+++ parent-cloud/Riboon-eureka-consumer/src/main/java/com/unv/service/RibbonService.java
@@ -1,5 +1,6 @@
 package com.unv.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,7 +12,12 @@ public class RibbonService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hi(String name){
         return restTemplate.getForObject("http://EUREKA-CLIENT-DEMO/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name){
+        return "hi,"+name+".sorry,error";
     }
 }
