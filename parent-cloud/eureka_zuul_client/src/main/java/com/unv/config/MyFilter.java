c09dac76fa75e7b51b88eccc59fcf05d12ffcb11
yuchengfan <yuchengfan321@163.com>
2022-05-21 10:32:02 +0800

    第一代网关zuul完结

---
+++ parent-cloud/eureka_zuul_client/src/main/java/com/unv/config/MyFilter.java
@@ -0,0 +1,46 @@
+package com.unv.config;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+@Component
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null){
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                return null;
+            }
+        }
+        return null;
+    }
+}
